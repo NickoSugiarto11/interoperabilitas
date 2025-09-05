cad31cd749a9ff72c8801e182b230afc2ac3586d
Nicko Sugiarto <nicko9995sgt@gmail.com>
2025-09-05 18:24:02 +0700

    update

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.listen(port, () => {
 
 //Array movies
 let movies = [
-    {id: 1, title: 'The Shawshank Redemption', year: 1994},
-    {id: 2, title: 'The Godfather', year: 1972},
-    {id: 3, title: 'The Dark Knight', year: 2008},
+    {id: 1, title: 'The Shawshank Redemption', director: 'Nicko Sugiarto', year: 1994},
+    {id: 2, title: 'The Godfather', director: 'Ferdy Sambo', year: 1972}, 
+    {id: 3, title: 'The Dark Knight', director: 'Ultraman', year: 2008},
 ];
 
 //Jika membuka localhost:3100 akan menampilkan pesan berikut
